0d1e622b2548d107092fc74b6b84a02897e986a8
Caio Fernandes <caiovf13@gmail.com>
2023-04-17 18:28:19 -0300

    📦 NEW: Add http api

--- http_api.tf
+++ http_api.tf
@@ -1,65 +1,64 @@
 resource "aws_apigatewayv2_api" "api" {
-  for_each                   = { for api_gateway in var.apis : api_gateway.name => api_gateway }
-  name                       = each.value.name
-  protocol_type              = try(each.value.protocol_type, "HTTP")
-  #  cors_configuration         = try(each.value.cors_configuration, "")
-  description                = try(each.value.description, "")
-  route_key                  = try(each.value.route_key, "")
-  target                     = try(each.value.target, "")
+  count                      = var.api_type == 'http' ? 1 : 0
+  name                       = var.name
+  protocol_type              = "HTTP"
+  #  cors_configuration         = try(var.cors_configuration, "")
+  description                = try(var.description, "")
+  route_key                  = try(var.route_key, "")
+  target                     = try(var.target, "")
 }
 
-
 resource "aws_apigatewayv2_integration" "http_integration" {
-  for_each           = { for integration in var.integrations : integration.name => integration }
-  api_id             = aws_apigatewayv2_api.api[each.value.api_name].id
-  integration_type   = each.value.integration_type
-  integration_method = each.value.integration_method
-  integration_uri    = each.value.integration_uri
+  count                      = var.api_type == 'http' ? 1 : 0
+  api_id             = aws_apigatewayv2_api.api[var.api_name].id
+  integration_type   = var.integration_type
+  integration_method = var.integration_method
+  integration_uri    = var.integration_uri
 }
 
 resource "aws_apigatewayv2_route" "integration_route" {
-  for_each  = { for integration in var.integrations : integration.name => integration }
-  api_id    = aws_apigatewayv2_api.api[each.value.api_name].id
-  route_key = each.value.route_key
+  count                      = var.api_type == 'http' ? 1 : 0
+  api_id    = aws_apigatewayv2_api.api[var.api_name].id
+  route_key = var.route_key
 
-  target = "integrations/${aws_apigatewayv2_integration.http_integration[each.value.name].id}"
+  target = "integrations/${aws_apigatewayv2_integration.http_integration[var.name].id}"
 }
 
 resource "aws_apigatewayv2_stage" "stage" {
-  for_each    = { for api_gateway in var.apis : api_gateway.name => api_gateway }
-  api_id      = aws_apigatewayv2_api.api[each.value.name].id
-  name        = "${local.workspace.environment_name}-stage"
+  count                      = var.api_type == 'http' ? 1 : 0
+  api_id      = aws_apigatewayv2_api.api[var.name].id
+  name        = "${var.environment_name}-stage"
   auto_deploy = true
 }
 
 resource "aws_apigatewayv2_domain_name" "domain_name" {
-  for_each    = { for domain in var.domains : domain.name => domain }
-  domain_name = "${each.value.name}.${each.value.domain_name}"
+  count       = var.api_type == 'http' ? 1 : 0
+  domain_name = "${var.name}.${var.domain_name}"
 
   domain_name_configuration {
-    certificate_arn = each.value.certificate_arn
+    certificate_arn = var.certificate_arn
     endpoint_type   = "REGIONAL"
     security_policy = "TLS_1_2"
   }
 }
 
 resource "aws_apigatewayv2_api_mapping" "domain_mapping" {
-  for_each        = { for domain in var.domains : domain.name => domain }
-  api_id          = aws_apigatewayv2_api.api[each.value.api_name].id
-  domain_name     = aws_apigatewayv2_domain_name.domain_name[each.value.name].id
-  stage           = aws_apigatewayv2_stage.stage[each.value.api_name].id
-  api_mapping_key = each.value.api_route_mapping
+  count                      = var.api_type == 'http' ? 1 : 0
+  api_id          = aws_apigatewayv2_api.api[var.api_name].id
+  domain_name     = aws_apigatewayv2_domain_name.domain_name[var.name].id
+  stage           = aws_apigatewayv2_stage.stage[var.api_name].id
+  api_mapping_key = var.api_route_mapping
 }
 
 resource "aws_route53_record" "hosted_zone" {
-  for_each    = { for domain in var.domains : domain.name => domain }
-  name    = aws_apigatewayv2_domain_name.domain_name[each.value.name].domain_name
+  count                      = var.api_type == 'http' ? 1 : 0
+  name    = aws_apigatewayv2_domain_name.domain_name[var.name].domain_name
   type    = "A"
-  zone_id = each.value.zone_id
+  zone_id = var.zone_id
 
   alias {
-    name                   = aws_apigatewayv2_domain_name.domain_name[each.value.name].domain_name_configuration[0].target_domain_name
-    zone_id                = aws_apigatewayv2_domain_name.domain_name[each.value.name].domain_name_configuration[0].hosted_zone_id
+    name                   = aws_apigatewayv2_domain_name.domain_name[var.name].domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.domain_name[var.name].domain_name_configuration[0].hosted_zone_id
     evaluate_target_health = false
   }
 }
