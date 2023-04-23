c75abf5cef8adeadea3d8a12e700d423f22b8019
Caio Fernandes <caiovf13@gmail.com>
2023-04-23 19:04:38 -0300

    👌 IMPROVE: Remove unused data

--- _data.tf
+++ _data.tf
@@ -6,11 +6,3 @@ data "aws_subnets" "current" {
   }
 }
 
-data "aws_acm_certificate" "current" {
-  domain   = var.hostZoneName
-  statuses = ["ISSUED"]
-}
-
-data "aws_route53_zone" "current" {
-  name = var.hostZoneName
-}
