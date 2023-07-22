ed1f950d7145bc552227cc34bde641cad6c21297
matthosking-mydeal <95453715+matthosking-mydeal@users.noreply.github.com>
2023-07-22 10:31:41 +1000

    fix typo

--- _outputs.tf
+++ _outputs.tf
@@ -4,7 +4,7 @@ output "api_gateway_domain_targets" {
       name = (var.api_type == "http"
         ? try(aws_apigatewayv2_domain_name.domain_name[domain.domain].domain_name_configuration[0].target_domain_name, "")
         : try(aws_api_gateway_domain_name.rest_domain_name[each.value.domain].hosted_zone_id, ""))
-      zone_id = (var.api_type == "http" ? 
+      zone_id = (var.api_type == "http"
         ? try(aws_apigatewayv2_domain_name.domain_name[domain.domain].domain_name_configuration[0].target_domain_name, "")
         : try(aws_api_gateway_domain_name.rest_domain_name[each.value.domain].regional_zone_id, ""))
     }
