4ee5edce3567b67b1f309387d430b5791facd635
Javad Khan <javad.khan@mydeal.com.au>
2023-08-03 21:24:14 +1000

    remove condition for dns target variable

--- _outputs.tf
+++ _outputs.tf
@@ -8,7 +8,6 @@ output "api_gateway_domain_targets" {
         ? try(aws_apigatewayv2_domain_name.domain_name[domain.domain].domain_name_configuration[0].target_domain_name, "")
         : try(aws_api_gateway_domain_name.rest_domain_name[domain.domain].regional_zone_id, ""))
     }
-    if var.create_dns_record && (var.api_type == "http" || var.api_type == "rest")
   }
   description = "Targets for creating DNS records manually"
 }
