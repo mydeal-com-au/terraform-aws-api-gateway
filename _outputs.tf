99c4c0f4c6e600548f0e0b1d2f5166bf48608b19
Javad Khan <javad.khan@mydeal.com.au>
2023-08-03 22:06:48 +1000

    fix domain name & hosted zone

--- _outputs.tf
+++ _outputs.tf
@@ -3,9 +3,9 @@ output "api_gateway_domain_targets" {
     {
       name = (var.api_type == "http"
         ? try(aws_apigatewayv2_domain_name.domain_name[domain.domain].domain_name_configuration[0].target_domain_name, "")
-        : try(aws_api_gateway_domain_name.rest_domain_name[domain.domain].hosted_zone_id, ""))
+        : try(aws_api_gateway_domain_name.rest_domain_name[domain.domain].regional_domain_name, ""))
       zone_id = (var.api_type == "http"
-        ? try(aws_apigatewayv2_domain_name.domain_name[domain.domain].domain_name_configuration[0].target_domain_name, "")
+        ? try(aws_apigatewayv2_domain_name.domain_name[domain.domain].domain_name_configuration[0].hosted_zone_id, "")
         : try(aws_api_gateway_domain_name.rest_domain_name[domain.domain].regional_zone_id, ""))
     }
   }
